679639650c9cdadf3cdf980960713b2e4760d97a
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-13 08:45:40 -0600

    created todo router get route

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -2,3 +2,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// GET routes
+
+router.get('/', (req,res) =>{
+    let queryText = 'SELECT * FROM todos ORDER BY id';
+
+    pool.query(queryText)
+        .then((results) =>{
+            console.log('query results: ', result);        
+            res.send(result.rows);
+        })
+        .catch((err) =>{
+            console.log('error making select query:', err);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
--- todos.md
+++ todos.md
@@ -4,8 +4,11 @@
 - [X] add and configure pool.js
 - [X] add and configure todo router
 - [X] configure route on server.js to todo router
-- [ ] create todo router get/post
+- [X] create todo router get route
+- [ ] create todo router post route
+- [ ] create todo router put route
 
 ##Client
 - [ ] configure input fields to send todo's to server
 - [ ] add ajax get and post
+- [ ] create function to append info to DOM
